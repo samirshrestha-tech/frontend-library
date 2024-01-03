42e03b858b8938a7c8de4e8ee2d73a83261ee414
samirshrestha-tech <128810359+samirshrestha-tech@users.noreply.github.com>
2024-01-03 23:12:47 +1100

    update on redux store

--- src/index.js
+++ src/index.js
@@ -8,11 +8,16 @@ import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.css";
 
+import { Provider } from "react-redux";
+import store from "./store";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/store.js
+++ src/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./pages/user_signup_login/userSlice";
 
 const store = configureStore({
   reducer: {
-    adminInfo: {},
+    adminInfo: userReducer,
   },
 });
 
