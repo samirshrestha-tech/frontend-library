0fc152ef11c05b3715dbdc24f5343bbc916d8691
samirshrestha-tech <128810359+samirshrestha-tech@users.noreply.github.com>
2024-01-06 23:59:39 +1100

    update on axios to vcall backend

--- src/axiosHelper.js
+++ src/axiosHelper.js
@@ -4,9 +4,17 @@ const rootEndPoint = process.env.REACT_APP_ROOTAPI;
 
 const userEndPoint = rootEndPoint + "/users";
 
+const getAccessJwt = () => {
+  return sessionStorage.getItem("AccessJwt");
+};
+
 const axiosProcessor = async (obj) => {
   try {
-    const resp = await axios(obj);
+    const resp = await axios(obj, {
+      headers: {
+        Authorization: getAccessJwt(),
+      },
+    });
     return resp.data;
   } catch (error) {
     return {
@@ -23,10 +31,19 @@ export const postAdminUser = async (data) => {
   });
 };
 
+// user api callss
+
 export const loginUser = async (data) => {
   return axiosProcessor({
     method: "post",
     url: userEndPoint + "/login",
     data,
+  });
+};
+
+export const getUser = async () => {
+  return axiosProcessor({
+    method: "get",
+    url: userEndPoint,
   });
 };
